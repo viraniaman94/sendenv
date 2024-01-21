5236829bf0a6d69cb3b284327e2ab4823f2a0e37
amanvirani <viraniaman94@gmail.com>
2024-01-20 22:06:09 -0500

    Refactor CLI commands and add variable functionality

--- cli.py
+++ cli.py
@@ -37,45 +37,67 @@ def list_vaults():
     for vault in vaults:
         print(vault)
 
-def add_variable(vault_name):
-    # Get all environment variables
-    env_vars = os.environ.keys()
-
-    # Create a completer with the environment variables
-    env_var_completer = FuzzyWordCompleter(env_vars)
-
+def add_variable(vault_name, variable_name=None):
     # Load the vault
     vault = VAULT_MANAGER.load_vault(vault_name)
 
-    # Prompt the user to select an environment variable
-    while True:
-        selected_var = prompt("Enter and select an environment variable (or type 'done' to finish): ", completer=env_var_completer)
-        if selected_var.lower() == 'done':
-            VAULT_MANAGER.save_vaults()
-            break
-        elif selected_var in env_vars:
-            # Add the selected variable to the vault
-            vault.add_variable(selected_var)
-        else:
-            print("Invalid selection. Please select a valid environment variable.")
-
-    print(f"Added selected variables to vault '{vault_name}'. The vault now contains:")
-    for var in vault.variables:
-        print(var)
-
-def delete_variable(vault_name, variable_name):
-    # Load the vault
-    vault = VAULT_MANAGER.load_vault(vault_name)
-
-    # Prompt the user for confirmation
-    confirm = input(f"Are you sure you want to delete variable '{variable_name}' from vault '{vault_name}'? (y/n): ")
-    if confirm.lower() == 'y':
-        # Delete the variable
-        vault.delete_variable(variable_name)
-
-        # Save the vault
-        VAULT_MANAGER.save_vault(vault)
-
-        print(f"Variable '{variable_name}' deleted from vault '{vault_name}'.")
+    if variable_name:
+        # If a variable name was provided, add it to the vault
+        vault.add_variable(variable_name)
+        print(f"Added variable '{variable_name}' to vault '{vault_name}'.")
     else:
-        print("Operation cancelled.")
+        # If no variable name was provided, prompt the user to add names
+        # Get all environment variables
+        env_vars = list(os.environ.keys())
+
+        # Create a completer with the environment variables
+        env_var_completer = FuzzyWordCompleter(env_vars)
+
+        # Prompt the user to select an environment variable
+        while True:
+            selected_var = prompt("Enter and select an environment variable (or type 'done' to finish): ", completer=env_var_completer)
+            if selected_var.lower() == 'done':
+                VAULT_MANAGER.save_vaults()
+                break
+            elif selected_var in env_vars:
+                # Add the selected variable to the vault
+                vault.add_variable(selected_var)
+                env_vars.remove(selected_var)
+            else:
+                print("Invalid selection. Please select a valid environment variable.")
+
+        print(f"Added selected variables to vault '{vault_name}'. The vault now contains:")
+        for var in vault.variables:
+            print(var)
+
+def delete_variable(vault_name, variable_name=None):
+    # Load the vault
+    vault = VAULT_MANAGER.load_vault(vault_name)
+
+    if variable_name:
+        # If a variable name was provided, delete it from the vault
+        vault.delete_variable(variable_name)
+        print(f"Deleted variable '{variable_name}' from vault '{vault_name}'.")
+    else:
+        # If no variable name was provided, prompt the user to delete names
+        # Get all variables in the vault
+        vault_vars = vault.variables
+
+        # Create a completer with the vault variables
+        vault_var_completer = FuzzyWordCompleter(vault_vars)
+
+        # Prompt the user to select a variable to delete
+        while True:
+            selected_var = prompt("Enter and select a variable to delete (or type 'done' to finish): ", completer=vault_var_completer)
+            if selected_var.lower() == 'done':
+                VAULT_MANAGER.save_vaults()
+                break
+            elif selected_var in vault_vars:
+                # Delete the selected variable from the vault
+                vault.delete_variable(selected_var)
+            else:
+                print("Invalid selection. Please select a valid variable.")
+
+        print(f"Deleted selected variables from vault '{vault_name}'. The vault now contains:")
+        for var in vault.variables:
+            print(var)
--- main.py
+++ main.py
@@ -9,31 +9,32 @@ def main():
     subparsers = parser.add_subparsers(dest='command')
 
     # Define the 'create' command
-    create_parser = subparsers.add_parser('create', help='Create a new vault')
+    create_parser = subparsers.add_parser('create-vault', help='Create a new vault')
     create_parser.add_argument('name', help='The name of the vault to create')
 
     # Define the 'delete' command
-    delete_parser = subparsers.add_parser('delete', help='Delete a vault')
+    delete_parser = subparsers.add_parser('delete-vault', help='Delete a vault')
     delete_parser.add_argument('name', help='The name of the vault to delete')
 
     # Define the 'list' command
-    list_parser = subparsers.add_parser('list', help='List all vaults')
+    list_parser = subparsers.add_parser('list-vaults', help='List all vaults')
 
     # Define the 'add' command
     add_parser = subparsers.add_parser('add-var', help='Add a variable to a vault')
     add_parser.add_argument('vault', help='The name of the vault')
+    add_parser.add_argument('variable', nargs='?', default=None, help='The name of the variable')
 
     # Define the 'delete-var' command
     delete_var_parser = subparsers.add_parser('delete-var', help='Delete a variable from a vault')
     delete_var_parser.add_argument('vault', help='The name of the vault')
-    delete_var_parser.add_argument('variable', help='The name of the variable')
+    delete_var_parser.add_argument('variable', nargs='?', default=None, help='The name of the variable')
 
     # Define the 'send' command
-    send_parser = subparsers.add_parser('send', help='Send a vault to a remote server')
+    send_parser = subparsers.add_parser('send-vault', help='Send a vault to a remote server')
     send_parser.add_argument('vault', help='The name of the vault to send')
 
     # Define the 'receive' command
-    receive_parser = subparsers.add_parser('receive', help='Receive a vault from a remote server')
+    receive_parser = subparsers.add_parser('receive-vault', help='Receive a vault from a remote server')
 
     # Define the 'list-var' command
     list_var_parser = subparsers.add_parser('list-var', help='List all variables in a vault')
@@ -43,14 +44,14 @@ def main():
     args = parser.parse_args()
 
     # Call the appropriate function based on the command
-    if args.command == 'create':
+    if args.command == 'create-vault':
         cli.create_vault(args.name)
-    elif args.command == 'delete':
+    elif args.command == 'delete-vault':
         cli.delete_vault(args.name)
-    elif args.command == 'list':
+    elif args.command == 'list-vaults':
         cli.list_vaults()
     elif args.command == 'add-var':
-        cli.add_variable(args.vault)
+        cli.add_variable(args.vault, args.variable)
     elif args.command == 'delete-var':
         cli.delete_variable(args.vault, args.variable)
     elif args.command == 'list-var':
