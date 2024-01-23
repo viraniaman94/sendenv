43542d8017c650a658e950b6634101451a2b4361
amanvirani <viraniaman94@gmail.com>
2024-01-22 19:40:22 -0500

    Bump version to 0.1.6 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sendenv',
-    version='0.1.5',
+    version='0.1.6',
     description='A tool for developers to share environment variables with their team over the internet.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
